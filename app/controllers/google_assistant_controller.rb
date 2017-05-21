e83b903589f9a3e40987dcc9473572cc3f81047d
Aaron Milam <aaron@lesson.ly>
2017-05-21 17:32:13 -0400

    Add hello world google assistant action

---
+++ app/controllers/google_assistant_controller.rb
@@ -0,0 +1,12 @@
+class GoogleAssistantController < ApplicationController
+
+  def conversation
+    assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
+      assistant.intent.main do
+        assistant.tell("<speak>Hello, world! I am the assistant you call when you're feeling productive and need something productive to do. Soon, you will be able to tell me what productive things you can do and ask me for recommendations as to what you could be doing. I am excited to help you out!</speak>")
+      end
+    end
+
+    render json: assistant_response
+  end
+end
