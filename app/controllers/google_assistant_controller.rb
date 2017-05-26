86dc6546bc92557033ca232ffc41b4451e892947
Aaron Milam <aaron@lesson.ly>
2017-05-26 08:38:25 -0400

    Don't need to verify CSRF token for Google Assistant actions

--- app/controllers/google_assistant_controller.rb
+++ app/controllers/google_assistant_controller.rb
@@ -1,4 +1,5 @@
 class GoogleAssistantController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def conversation
     assistant_response = GoogleAssistant.respond_to(params, response) do |assistant|
